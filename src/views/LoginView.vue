a130cf5edabd20206dfdb54645dc78b5012052b3
Onur Evren <onur.evren.1999@gmail.com>
2023-01-09 12:01:41 +0300

    LoginView Added

---
+++ src/views/LoginView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="container-fluid vh-100" style="margin-top:300px">
+      <div class="" style="margin-top:200px">
+        <div class="rounded d-flex justify-content-center">
+          <div class="col-md-4 col-sm-12 shadow-lg p-5 bg-light">
+            <div class="text-center">
+              <h3 class="text-primary">Giriş Yap</h3>
+            </div>
+            <form action="">
+              <div class="p-4">
+                <div class="input-group mb-3">
+                                    <span class="input-group-text bg-primary"><i
+                                        class="bi bi-person-plus-fill text-white"></i></span>
+                  <input type="text" class="form-control" placeholder="E-Posta Adresi">
+                </div>
+                <div class="input-group mb-3">
+                                    <span class="input-group-text bg-primary"><i
+                                        class="bi bi-key-fill text-white"></i></span>
+                  <input type="password" class="form-control" placeholder="Şifre">
+                </div>
+
+                <div class="d-flex justify-content-center">
+                  <button class="btn btn-primary">Giriş Yap</button>
+                </div>
+                <p class="text-center mt-5">Bir Hesabın Yokmu?
+                  <span class="text-primary">Kayıt Ol</span>
+                </p>
+                <p class="text-center text-primary">Şifremi Unuttum</p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "LoginView"
+}
+</script>
+<style scoped>
+</style>
